33c04b6566d842fc57667ddf70588223a76cdb72
Bruno Oliveira <96549400+DevBrunoo@users.noreply.github.com>
2025-07-28 11:41:09 +0000

    s

--- ufc-theme-WP/footer.php
+++ ufc-theme-WP/footer.php
@@ -2,7 +2,7 @@
     <p>&copy; <?php echo date("Y"); ?> UFC Theme. All rights reserved.</p>
     <nav>
         <ul>
-            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#"> Privacy Policy</a></li>
             <li><a href="#">Terms of Service </a></li>
             <li><a href="#">Contact  Us</a></li>
         </ul>
