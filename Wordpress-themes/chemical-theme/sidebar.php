8d2f1c2f716aef18aaa8e81312a323a86f91da0d
Bruno Oliveira <96549400+DevBrunoo@users.noreply.github.com>
2025-08-23 14:16:29 +0000

    d

--- Wordpress-themes/chemical-theme/sidebar.php
+++ Wordpress-themes/chemical-theme/sidebar.php
@@ -1,5 +1,5 @@
 <div class="sidebar">
-    <h2>Chemical Sidebar</h2>
+    <h2>Chemical Sidebar, ok</h2>
     <ul>
         <li><a href="#">Chemical Safety</a></li>
         <li><a href="#">Chemical Reactions</a></li>
